7f8b0f8695ba63ef7d092534f366045d76ab93fb
Dat Dinh <dinh3332@stthomas.edu>
2025-09-02 18:40:12 -0500

    Try running on different server

---
+++ tests/AssortativeTest/build/CMakeFiles/AssortativeTest.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "../AssortativeTest"
+  "../AssortativeTest.pdb"
+  "CMakeFiles/AssortativeTest.dir/AssortativeNeonatesTest.cu.o"
+  "CMakeFiles/AssortativeTest.dir/AssortativeNeonatesTest.cu.o.d"
+  "CMakeFiles/AssortativeTest.dir/GenPhenMapTest.cu.o"
+  "CMakeFiles/AssortativeTest.dir/GenPhenMapTest.cu.o.d"
+  "CMakeFiles/AssortativeTest.dir/cmake_device_link.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CUDA)
+  include(CMakeFiles/AssortativeTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tests/AssortativeTest/build/CMakeFiles/AssortativeTest.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for AssortativeTest.
+# This may be replaced when dependencies are built.
