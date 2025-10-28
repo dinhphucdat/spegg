efa324f6a0980adc32e2fb7ac19d7661de37e566
Dat Dinh <dinh3332@stthomas.edu>
2025-10-28 10:49:54 -0500

    Add Python direct parameter passing to avoid the IO requests - TRIAL 14

--- src/cuda/species/add_kids/genotype_phenotype_map_parameters.cu
+++ src/cuda/species/add_kids/genotype_phenotype_map_parameters.cu
@@ -44,7 +44,7 @@ void GenotypePhenotypeMapParameters::processDemeSpecificParameters(const py::arr
 	if (buf.shape.size() != 2) {
 		throw std::runtime_error("The array is not a strict 2d array!");
 	}
-	if (buf.shape[0] != this.Number_of_Parameters) {
+	if (buf.shape[0] != Number_of_Parameters) {
 		throw std::runtime_error(
 			"Number of parameters and the first dimension of array (size of number of parameters) are not the same!");
 	}
