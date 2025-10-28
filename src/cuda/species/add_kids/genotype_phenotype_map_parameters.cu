0a0dabb291a68bb99b42f6578933071320d7b665
Dat Dinh <dinh3332@stthomas.edu>
2025-10-28 09:50:37 -0500

    Add Python direct parameter passing to avoid the IO requests - TRIAL 4 ONLY

--- src/cuda/species/add_kids/genotype_phenotype_map_parameters.cu
+++ src/cuda/species/add_kids/genotype_phenotype_map_parameters.cu
@@ -46,8 +46,7 @@ void processDemeSpecificParameters(const py::array_t<float>& demeSpecificParams)
 	}
 	if (buf.shape[0] != Number_of_Parameters) {
 		throw std::runtime_error(
-			"Number of parameters and the first dimension of array" +
-			"(size of number of parameters) are not the same!");
+			"Number of parameters and the first dimension of array (size of number of parameters) are not the same!");
 	}
 	this->Number_of_Demes = static_cast<int>(buf.shape[1]);
 	deme_specific_parameters = new thrust::device_vector<float>[Number_of_Parameters];
